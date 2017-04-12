a8f1423e0bcd0b6569bcaceb27d38ec9c556f01c
Frederik Vosberg <hello@frederikvosberg.de>
2017-04-12 18:59:48 +0200

    [BUGFIX] corrected overwrite

--- 2017-03-29/aufgabe13.py
+++ 2017-03-29/aufgabe13.py
@@ -1,4 +1,13 @@
 #!/usr/local/bin/python3.6
 
-for i in [2,3,5,7,11,13]:
-    print(i, i**2, sep=":")
+for i in range(1,11):
+    print(i)
+
+for i in range(-10, 11):
+    print(i)
+
+for i in range(10, -1, -1):
+    print(i)
+
+for i in range(1, 22, 2):
+    print(i)
